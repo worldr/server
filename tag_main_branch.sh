19f87829314d3701d0dc47d2191a4032e486693d
Michael Antipin <Aurora12@users.noreply.github.com>
2020-10-28 19:27:59 +0300

    Checking that auto-tagging works fine (#56)
    * added auto-versioning
    
    * removed changes that get merged in another branch
    
    * Bump version: 1.6.0 → 1.6.1
    
    * minor changes in tagging script

--- tag_main_branch.sh
+++ tag_main_branch.sh
@@ -4,21 +4,22 @@ MAIN_BRANCH=$1
 if [ -z "$MAIN_BRANCH" ]; then
 	echo Usage: tag_main_branch MAIN_BRANCH_NAME
 	echo Error: main branch name parameter is missing!
-	exit	
+	exit 1
 fi
 
-echo INDENTED FOR USE WITH A GITHUB ACTION 
+echo INTENDED FOR USE WITH A GITHUB ACTION 
 echo TRIGGERED BY A PR MERGE INTO THE MAIN BRANCH \("$MAIN_BRANCH"\)!
 
 BRANCH=$(git symbolic-ref -q --short HEAD)
 if [ "$BRANCH" != "$MAIN_BRANCH" ]; then
 	echo Error: you are not on the main branch of the repo \("$MAIN_BRANCH"\)!
-	exit
+	exit 2
 fi
 
 V=$(cat ./VERSION.txt)
-EXISTS=$(git tag|grep "$V")
-if [[ -z "$EXISTS" ]]; then
+EXISTING=$(git tag|grep "$V")
+echo Current version is "$V", existing version is "$EXISTING".
+if [[ -z "$EXISTING" ]]; then
 	git tag "v$V"
 	git push origin "v$V"
 else
